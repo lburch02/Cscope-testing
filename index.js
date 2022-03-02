adda8ef044af6b835b7d16484a051a27eefae37e
Luke B <lukeburch1234@gmail.com>
2022-03-02 15:59:34 +0000

    Looking into .lock file not deleting

--- index.js
+++ index.js
@@ -1,140 +1,51 @@
-var newTeam = {"name":"Regional Office","members":[{"user":"","team":"Local Office","role":"member","status":"active"},{"user":"simonfry.cscope.info.test1-at-gmail.com","team":"","role":"leader","status":"active"}]}
-
-var beforeTeams = [
-{
-    "name": "Head Office",
-    "members": [
-        {
-            "user": "",
-            "team": "Regional Office",
-            "role": "member",
-            "status": "active"
-        },
-        {
-            "user": "simonfry.cscope.info.test-at-gmail.com",
-            "team": "",
-            "role": "leader",
-            "status": "active"
-        }
-    ]
-}
-,
-{
-    "name": "Regional Office",
-    "members": [
-        {
-            "user": "",
-            "team": "Local Office",
-            "role": "member",
-            "status": "active"
-        },
-        {
-            "user": "simonfry.cscope.info.test-at-gmail.com",
-            "team": "",
-            "role": "leader",
-            "status": "active"
-        },
-        {
-            "user": "simonfry.cscope.info.test1-at-gmail.com",
-            "team": "",
-            "role": "leader",
-            "status": "active"
-        }
-    ]
-}
-,
-{
-    "name": "Team B",
-    "members": [
-        {
-            "user": "lbtester11-at-icloud.com",
-            "team": "",
-            "role": "member",
-            "status": "active"
-        },
-        {
-            "user": "lukeburch1234-at-gmail.com",
-            "team": "",
-            "role": "member",
-            "status": "active"
-        },
-        {
-            "user": "yachtsamba.samba-at-gmail.com",
-            "team": "",
-            "role": "leader",
-            "status": "active"
-        }
-    ]
-}
-,
-{
-    "name": "Team A",
-    "members": [
-        {
-            "user": "lbtester1-at-icloud.com",
-            "team": "",
-            "role": "member",
-            "status": "active"
-        },
-        {
-            "user": "lukeburch1234-at-gmail.com",
-            "team": "",
-            "role": "leader",
-            "status": "active"
-        },
-        {
-            "user": "yachtsamba.samba-at-gmail.com",
-            "team": "",
-            "role": "leader",
-            "status": "active"
-        }
-    ]
-}
-,
-{
-    "name": "Local Office",
-    "members": [
-        {
-            "user": "simonfry.cscope.info.test2-at-gmail.com",
-            "team": "",
-            "role": "leader",
-            "status": "active"
-        }
-    ]
-}
-]
-
-var teamFound = false;
-
-for (var team in beforeTeams) { //loop through all teams in ddb
-		if (beforeTeams[team].name==newTeam.name) { // if current iterating team matches api call team
-			teamFound=true;
-			for (var newMember in newTeam.members) { //iterate through api call team members
-				var memberExists=false;
-				for (var beforeMember in beforeTeams[team].members) { //iterate through ddb version of api call team members
-					if (beforeTeams[team].members[beforeMember].user==newTeam.members[newMember].user
-						&& beforeTeams[team].members[beforeMember].team==newTeam.members[newMember].team
-						) {
-						memberExists=true;
-						if (newTeam.members[newMember].status=="resend"
-							&& beforeTeams[team].members[beforeMember].status!="active") {
-							// await teamFunctions.sendInvite(newTeam, newTeam.members[newMember], username);
-						} else {
-							newTeam.members[newMember].status=beforeTeams[team].members[beforeMember].status;
-						}
-						console.log("BB \n")
-						console.log(beforeTeams[team])	
-						beforeTeams[team].members.splice(beforeMember,1);
-						console.log(beforeTeams[team])
-						break;
-					}
-				}
-				if (!memberExists) {
-					// await teamFunctions.sendInvite(newTeam, newTeam.members[newMember], username);
-				}
+var data = {
+	"IsTruncated": false,
+	"Marker": "",
+	"Contents": [
+		{
+			"Key": "Waiting/reecestafford-at-cscope.co.uk/1646133718415/Europe-London/275856 MXL4  [From 01-04-18 to 09-07-18] [09072018145121].cdf",
+			"LastModified": "2022-03-01T11:22:00.000Z",
+			"ETag": "\"4b6c23fa7d6df31165c0b55142a0e697\"",
+			"Size": 1194,
+			"StorageClass": "STANDARD",
+			"Owner": {
+				"ID": "5c33ef4c1258ed508b6eba835c6888e653597a98360226e6499418296a576f4f"
 			}
-			// teamFunctions.deleteTeam(beforeTeams[team]);
-			// console.log(beforeTeams[team])
+		},
+		{
+			"Key": "Waiting/reecestafford-at-cscope.co.uk/1646133718415/Europe-London/275856 MXL4  [From 01-04-18 to 09-07-18] [09072018145121].cdf.lock",
+			"LastModified": "2022-03-01T11:22:00.000Z",
+			"ETag": "\"d41d8cd98f00b204e9800998ecf8427e\"",
+			"Size": 0,
+			"StorageClass": "STANDARD",
+			"Owner": {
+				"ID": "5c33ef4c1258ed508b6eba835c6888e653597a98360226e6499418296a576f4f"
+			}
+		}
+	],
+	"Name": "dev1-cscope.info.cdf.parts",
+	"Prefix": "Waiting/",
+	"MaxKeys": 1000,
+	"CommonPrefixes": []
+}
+
+var contents = data.Contents.sort(function(lhs, rhs) {
+	return lhs.Size - rhs.Size;
+})
+
+// console.log(contents)
+
+for (var loadFileIndex in contents) {
+	var thisLoadFileKey = contents[loadFileIndex].Key;
+	for (var lockFileIndex in contents) {
+		var thisLockFileKey = contents[lockFileIndex].Key;
+		if (!thisLoadFileKey.includes(".lock") && thisLoadFileKey + ".lock" === thisLockFileKey) {
+			contents.splice(loadFileIndex, 1); // remove this item, it is locked
+			contents.splice(lockFileIndex, 1); // remove the lock for this item, it has been checked
 			break;
 		}
+	}
 }
+
+console.log(contents)
+
